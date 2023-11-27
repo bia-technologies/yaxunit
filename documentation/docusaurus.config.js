1e5d8cfe638be545c9794cc15f31937f2c98b837
alkoleft <alkoleft@gmail.com>
2023-11-27 12:03:08 +0300

    Обновление документации
    Обновлена версия docusaurus
    Добавлены страницы с описанием событий + шапаргалка

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -168,8 +167,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} BIA Technologies, Inc. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
       },
     }),
     markdown: {
