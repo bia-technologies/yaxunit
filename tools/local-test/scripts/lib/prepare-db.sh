f2a4f860a3b7eae66b55caa22cfc46a121f5039c
alkoleft <alkoleft@gmail.com>
2024-12-19 00:33:20 +0300

    tests: корректировака скрипта локального тестирования

--- tools/local-test/scripts/lib/prepare-db.sh
+++ tools/local-test/scripts/lib/prepare-db.sh
@@ -1,29 +1,31 @@
 CONNECTION_STRING=$1
 JOB_NAME=$2
 
-BASE_ARGUMENTS="$CONNECTION_STRING /DisableStartupDialogs /DisableStartupMessages /Out $TMP_PATH/$JOB_NAME.log"
+. $LIB_PATH/print.sh
+
+BASE_ARGUMENTS="$CONNECTION_STRING /DisableStartupDialogs /DisableStartupMessages /Out $LOGS_PATH/$JOB_NAME.log"
 DESIGNER_COMMAND="$ONEC_PATH/1cv8 DESIGNER $BASE_ARGUMENTS"
 
-echo "=========== Загрузка конфигурации ($JOB_NAME)"
-$DESIGNER_COMMAND /LoadCfg $BINARY_PATH/configuration.cf /UpdateDBCfg >&- 2>&-
+$DESIGNER_COMMAND /LoadCfg $BINARY_PATH/configuration.cf /UpdateDBCfg  >> $LOGS_PATH/$JOB_NAME.log 2>&1
 SUCCESS=$?
+print_state "Загрузка конфигурации в ${BOLD}$JOB_NAME${RESET}"
 # cat $TMP_PATH/$JOB_NAME.log
 
 EXTENSIONS_SET=("smoke" "tests" "yaxunit")
 
 for key in "${EXTENSIONS_SET[@]}";do
     if [[ $SUCCESS -eq 0 ]]; then
-        echo "=========== Загрузка расширения $key ($JOB_NAME)"
-        $DESIGNER_COMMAND /LoadCfg $BINARY_PATH/$key.cfe -Extension $key /UpdateDBCfg >&- 2>&-
+        $DESIGNER_COMMAND /LoadCfg $BINARY_PATH/$key.cfe -Extension $key /UpdateDBCfg  >> $LOGS_PATH/$JOB_NAME.log 2>&1
         SUCCESS=$?
+        print_state "Загрузка расширения $key в ${BOLD}$JOB_NAME${RESET}"
         # cat $TMP_PATH/$JOB_NAME.log
     fi
 done
 
 if [[ $SUCCESS -eq 0 ]]; then
-    echo "=========== Снятие безопасного режима ($JOB_NAME)"
-    $ONEC_PATH/1cv8 ENTERPRISE $BASE_ARGUMENTS /Execute $LIB_PATH/DisableSafeMode.epf >&- 2>&-
+    $ONEC_PATH/1cv8 ENTERPRISE $BASE_ARGUMENTS /Execute $LIB_PATH/DisableSafeMode.epf  >> $LOGS_PATH/$JOB_NAME.log 2>&1
     SUCCESS=$?
+    print_state "Снятие безопасного режима ${BOLD}$JOB_NAME${RESET}"
     # cat $TMP_PATH/$JOB_NAME.log
 fi
 
--- tools/local-test/scripts/lib/print.sh
+++ tools/local-test/scripts/lib/print.sh
@@ -1,20 +1,31 @@
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
 NC='\033[0m'
+RESET=$(tput sgr0)
+BOLD=$(tput bold)
+
+function print_state {
+    if [[ $? -eq 0 ]]; then
+        echo -e "  ${GREEN}✓  ${RESET} $1. ${GREEN}Успешно${RESET}"
+    else
+        echo -e "  ${RED}×  ${RESET} $1. ${RED}Не удалось${RESET}"
+    fi
+}
 
 function print_success {
-    echo -e "  ${GREEN}🟢 ${NC} $1"
+    echo -e "  ${GREEN}🟢 ${RESET} $1"
 }
 
 function print_error {
-    echo -e "  ${RED}🔴 ${NC} $1 "
+    echo -e "  ${RED}🔴 ${RESET} $1 "
 }
 
 function print_info {
-    echo -e "  ${GREEN}🛈  ${NC} $1"
+    echo -e "  ${BLUE}🛈  ${RESET} $1"
 }
 
 function print_warn {
-    echo -e "  ${YELLOW}🟠 ${NC} $1"
+    echo -e "  ${YELLOW}🟠 ${RESET} $1"
 }
